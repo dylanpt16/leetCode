5ba8bf85d115e48d763e94fef571f0e2badcd856
Dylan Chan <dylanchan@Phuongs-MacBook-Pro.local>
2018-11-22 18:07:03 -0800

    twoSum.js

---
+++ twoSum.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+  const hashTable = {};
+
+  for(let i = 0; i <= nums.length; i++) {
+    fistNum = target - nums[i];
+    if(hashTable[]:) {
+    }else {
+    }
+  }
+};
